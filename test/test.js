abf1fcc86fe4c2e3c960a97ef4ef1bf3922135ab
abatool-abatool <amna.deakin@gmail.com>
2022-12-13 15:48:58 +1100

    week 6

---
+++ test/test.js
@@ -0,0 +1,24 @@
+let express = require("express");
+var expect = require("chai").expect; 
+var request = require("request"); 
+
+describe("Add Two Numbers", function()
+{
+    var url = 'http://localhost:8080/addTwoNumbers/3/5';
+    //first it function....
+    it("return status 200 to check if api works", function(done){
+        request(url, function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done()
+        });
+    });
+
+    //second it function....
+    it("returns statusCode key in body to check if api give right result should be 200", function(done) {
+        request(url, function(error, response, body) {
+            body = JSON.parse(body)
+            expect(body.statusCode).to.equal(200);
+            done()
+            });
+    });
+});
